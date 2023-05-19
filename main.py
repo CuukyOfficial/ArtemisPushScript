c7862604cb66a171e4ea5d7004c3e16f5af81344
CuukyOfficial <just.cookie.jc@gmail.com>
2023-05-19 14:44:04 +0200

    Reorder some stuff

--- main.py
+++ main.py
@@ -33,16 +33,14 @@ while True:
 
 extra_git_commands = []
 # Check if user wants extra git inputs
+print("Now enter extra git commands that will be executed in every cloned directory (leave empty if finished): ")
 while True:
-    print("Now enter extra git commands that will be executed in every cloned directory (leave empty if finished): ")
     command = input()
     if command == "":
         break
     extra_git_commands.append(command)
 
 for key in types:
-    git_url = input("Enter git url for " + types[key] + ": ")
-
     source_dir = os.path.join(directory, key)
 
     # Check if directory exists
@@ -57,6 +55,8 @@ for key in types:
         print("Directory " + working_dir_temp + " already exists, skipping...")
         continue
 
+    git_url = input("Enter git url for " + types[key] + ": ")
+
     # Clone git repository
     os.system("git clone " + git_url + " " + working_dir_temp)
 
@@ -68,9 +68,9 @@ for key in types:
     os.system("rm -rf " + os.path.join(working_dir_temp, "*"))
 
     # Copy files from task directory to repository
-    os.system("cp -r " + os.path.join(source_dir, "*") + working_dir_temp)
+    os.system("cp -r " + os.path.join(source_dir, "*") + " " + working_dir_temp)
 
-    success = input("Please enter if the directory at " + working_dir_temp + "is complete and ready to push (y/n): ")
+    success = input("Please enter if the directory at " + working_dir_temp + " is complete and ready to push (y/n): ")
     if success == "n" or success == "N":
         print("Skipping git add, commit and push for " + key)
         continue
