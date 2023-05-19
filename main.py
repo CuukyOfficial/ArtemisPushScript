250b567cb1511606c7a3cea00f9cf1febdf09aad
CuukyOfficial <just.cookie.jc@gmail.com>
2023-05-19 14:02:55 +0200

    Removed os specific path separator

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ types = {
     "Tests": "tests"
 }
 
-print("ArtemisTransferScript v0.1 by Cuuky")
+print("ArtemisTransferScript v0.1.1 by Cuuky")
 print("NOTE: The script will only work if you have a valid Artemis task directory. "
       "The directory should contain the following folders:")
 
@@ -41,41 +41,43 @@ while True:
     extra_git_commands.append(command)
 
 for key in types:
-    git_url = input("Enter git url for " + key + ": ")
+    git_url = input("Enter git url for " + types[key] + ": ")
+
+    source_dir = os.path.join(directory, key)
 
     # Check if directory exists
-    if not os.path.isdir(directory + "/" + key):
-        print("Directory " + directory + "/" + key + " does not exist, skipping...")
+    if not os.path.isdir(source_dir):
+        print("Directory " + source_dir + " does not exist, skipping...")
         continue
 
+    working_dir_temp = os.path.join(working_dir, types[key])
+
     # Check if git repository exists
-    if os.path.isdir(working_dir + "/" + key):
-        print("Directory " + working_dir + "/" + types[key] + " already exists, skipping...")
+    if os.path.isdir(working_dir_temp):
+        print("Directory " + working_dir_temp + " already exists, skipping...")
         continue
 
     # Clone git repository
-    os.system("git clone " + git_url + " " + working_dir + "/" + types[key])
-
-    working_dir_temp = working_dir + "/" + types[key]
+    os.system("git clone " + git_url + " " + working_dir_temp)
 
     # Execute extra git commands
     for command in extra_git_commands:
         os.system("cd " + working_dir_temp + " && " + command)
 
     # Delete files in repository
-    os.system("rm -rf " + working_dir + "/" + types[key] + "/*")
+    os.system("rm -rf " + os.path.join(working_dir_temp, "*"))
 
     # Copy files from task directory to repository
-    os.system("cp -r " + directory + "/" + key + "/* " + working_dir_temp)
+    os.system("cp -r " + os.path.join(source_dir, "*") + working_dir_temp)
 
     # Add files to git
-    os.system("cd " + working_dir_temp + "/" + types[key] + " && git add .")
+    os.system("cd " + working_dir_temp + " && git add .")
 
     # Commit files
-    os.system("cd " + working_dir_temp + "/" + types[key] + " && git commit -m \"Initial commit\"")
+    os.system("cd " + working_dir_temp + " && git commit -m \"Initial commit\"")
 
     # Push files
-    os.system("cd " + working_dir_temp + "/" + types[key] + " && git push")
+    os.system("cd " + working_dir_temp + " && git push")
 
     print("Successfully cloned " + key + " repository")
 
